91787d560df72e658e677c971df871556a7cce16
Kevin Muchene <muchenekevin@gmail.com>
2023-10-15 09:12:07 -0500

    Removed un-necessary imports

--- src/Components/ResuableComponents/SearchComponent.jsx
+++ src/Components/ResuableComponents/SearchComponent.jsx
@@ -1,13 +1,9 @@
-import * as React from "react";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
+import React from "react";
 import Divider from "@mui/material/Divider";
 import { Box, Grid, TextField, styled } from "@mui/material";
 
 const CustomBox = styled(Box)({
   padding: 2,
-  // alignItems: "space-around",
 });
 
 export default function SearchComponent() {
