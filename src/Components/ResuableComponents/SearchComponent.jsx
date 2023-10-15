46b45d6645dc45fd2c1bd4430586123b5bc8a4b3
Kevin Muchene <muchenekevin@gmail.com>
2023-10-15 11:28:47 -0500

    Updated the state to handel search query

--- src/Components/ResuableComponents/SearchComponent.jsx
+++ src/Components/ResuableComponents/SearchComponent.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Divider from "@mui/material/Divider";
-import { Box, Grid, TextField, styled } from "@mui/material";
+import { Box, Button, Grid, TextField, styled } from "@mui/material";
 
 const CustomBox = styled(Box)({
   padding: 2,
 });
 
-export default function SearchComponent() {
+export default function SearchComponent({
+  searchQuery,
+  setSearchQuery,
+  handleSearch,
+}) {
+  const [localSearchQuery, setLocalSearchQuery] = useState("");
+
+  const handleSubmit = () => {
+    handleSearch(localSearchQuery);
+    setLocalSearchQuery("");
+  };
   return (
     <CustomBox sx={{ flexGrow: 1 }}>
       <Grid
@@ -14,14 +24,21 @@ export default function SearchComponent() {
         spacing={2}
         style={{ marginBottom: "0.5em", marginTop: "0.5em" }}
       >
-        <Grid item xs={12} md={12}>
+        <Grid item xs={12} md={8}>
           <TextField
             id="standard-search"
             label="Search Car"
             type="search"
             variant="standard"
             fullWidth
+            value={localSearchQuery}
+            onChange={(e) => setLocalSearchQuery(e.target.value)}
           />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Search
+          </Button>
         </Grid>
       </Grid>
       <Divider orientation="horizontal" flexItem />
