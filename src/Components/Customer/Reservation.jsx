798a602e581398802962480f0100f8ca3de0a4ce
Kevin Muchene <muchenekevin@gmail.com>
2023-10-21 09:32:19 -0500

    Implemented show alert when Pickup button is clicked

--- src/Components/Customer/Reservation.jsx
+++ src/Components/Customer/Reservation.jsx
@@ -1,5 +1,5 @@
 import Container from "@mui/material/Container";
-import { Grid, Button } from "@mui/material";
+import { Grid, Button, Alert } from "@mui/material";
 import ViewCar from "../ResuableComponents/ViewCar";
 import { useEffect, useState } from "react";
 import NotReserved from "./NotReserved";
@@ -38,12 +38,23 @@ let carInfo = [
 export default function Reservation() {
   const [carReserved, setCarReserved] = useState(carInfo);
   const [status, setStatus] = useState(true);
+  const [showAlert, setShowAlert] = useState(false);
 
   useEffect(() => {
     if (carInfo.length === 0) {
       setStatus(false);
     }
   }, []);
+
+  useEffect(() => {
+    if (showAlert) {
+      const timer = setTimeout(() => {
+        setShowAlert(false);
+      }, 4000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [showAlert]);
 
   const handleSubmit = (value) => {
     carInfo = carInfo.filter((car) => car.make !== value);
@@ -56,31 +67,52 @@ export default function Reservation() {
   };
   // debugger;
   return (
-    <Grid container="true" sx={{ padding: "1em" }} spacing={3}>
-      {status ? (
-        <>
-          {carReserved.map((car, key) => (
-            <Grid key={key} item md={3}>
-              <ViewCar car={car} medsize={12}>
-                <Grid item md={12}>
-                  <Button
-                    variant="outlined"
-                    onClick={() => handleSubmit(car.make)}
-                  >
-                    Return
-                  </Button>
-                </Grid>
-              </ViewCar>
-            </Grid>
-          ))}
-        </>
-      ) : (
+    <>
+      {showAlert && (
         <Container>
-          <Grid item md={12}>
-            <NotReserved />
-          </Grid>
+          <Alert severity="success" variant="filled">
+            Awesome! Enjoy the ride
+          </Alert>
         </Container>
       )}
-    </Grid>
+
+      <Grid container="true" sx={{ padding: "1em" }} spacing={3}>
+        {status ? (
+          <>
+            {carReserved.map((car, key) => (
+              <Grid key={key} item md={3}>
+                <ViewCar car={car} medsize={12}>
+                  <Grid item md={6}>
+                    <Button
+                      variant="outlined"
+                      onClick={() => handleSubmit(car.make)}
+                    >
+                      Return
+                    </Button>
+                  </Grid>
+                  <Grid item md={6}>
+                    <Button
+                      variant="outlined"
+                      onClick={() => {
+                        setShowAlert(true);
+                        handleSubmit(car.make);
+                      }}
+                    >
+                      Pickup
+                    </Button>
+                  </Grid>
+                </ViewCar>
+              </Grid>
+            ))}
+          </>
+        ) : (
+          <Container>
+            <Grid item md={12}>
+              <NotReserved />
+            </Grid>
+          </Container>
+        )}
+      </Grid>
+    </>
   );
 }
